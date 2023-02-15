50176e9b04822abeb6da7491bed67b58ebed75d6
felipeymn <felipe.ymm@gmail.com>
2023-02-14 23:24:25 -0300

    feat: update lualine bg color

--- lua/lualine/themes/florence.lua
+++ lua/lualine/themes/florence.lua
@@ -1,41 +1,42 @@
 local colors = {
-	bg = nil,
-	alt_fg = "#0c0d0a",
-	yellow = "#d4ae5c",
-	blue = "#9fcdd9",
-	cyan = "#78beab",
-	red = "#d9573d",
-	green = "#9bb02e",
+  bg = nil,
+  alt_fg = "#0c0d0a",
+  yellow = "#d4ae5c",
+  blue = "#9fcdd9",
+  cyan = "#78beab",
+  red = "#d9573d",
+  green = "#9bb02e",
+  dark = "#0e0f0c",
 }
 return {
-	normal = {
-		a = { bg = colors.yellow, fg = colors.alt_fg, gui = "bold" },
-		b = { bg = colors.bg, fg = colors.bg },
-		c = { bg = colors.bg, fg = colors.bg },
-	},
-	insert = {
-		a = { bg = colors.blue, fg = colors.alt_fg, gui = "bold" },
-		b = { bg = colors.bg, fg = colors.bg },
-		c = { bg = colors.bg, fg = colors.bg },
-	},
-	visual = {
-		a = { bg = colors.cyan, fg = colors.alt_fg, gui = "bold" },
-		b = { bg = colors.bg, fg = colors.bg },
-		c = { bg = colors.bg, fg = colors.bg },
-	},
-	replace = {
-		a = { bg = colors.red, fg = colors.alt_fg, gui = "bold" },
-		b = { bg = colors.bg, fg = colors.bg },
-		c = { bg = colors.bg, fg = colors.bg },
-	},
-	command = {
-		a = { bg = colors.green, fg = colors.alt_fg, gui = "bold" },
-		b = { bg = colors.bg, fg = colors.bg },
-		c = { bg = colors.bg, fg = colors.bg },
-	},
-	inactive = {
-		a = { bg = colors.bg, fg = colors.bg, gui = "bold" },
-		b = { bg = colors.bg, fg = colors.bg },
-		c = { bg = colors.bg, fg = colors.bg },
-	},
+  normal = {
+    a = { bg = colors.yellow, fg = colors.alt_fg, gui = "bold" },
+    b = { bg = colors.dark, fg = colors.dark },
+    c = { bg = colors.dark, fg = colors.dark },
+  },
+  insert = {
+    a = { bg = colors.blue, fg = colors.alt_fg, gui = "bold" },
+    b = { bg = colors.dark, fg = colors.dark },
+    c = { bg = colors.dark, fg = colors.dark },
+  },
+  visual = {
+    a = { bg = colors.cyan, fg = colors.alt_fg, gui = "bold" },
+    b = { bg = colors.dark, fg = colors.dark },
+    c = { bg = colors.dark, fg = colors.dark },
+  },
+  replace = {
+    a = { bg = colors.red, fg = colors.alt_fg, gui = "bold" },
+    b = { bg = colors.dark, fg = colors.dark },
+    c = { bg = colors.dark, fg = colors.dark },
+  },
+  command = {
+    a = { bg = colors.green, fg = colors.alt_fg, gui = "bold" },
+    b = { bg = colors.dark, fg = colors.dark },
+    c = { bg = colors.dark, fg = colors.dark },
+  },
+  inactive = {
+    a = { bg = colors.dark, fg = colors.dark, gui = "bold" },
+    b = { bg = colors.dark, fg = colors.dark },
+    c = { bg = colors.dark, fg = colors.dark },
+  },
 }
