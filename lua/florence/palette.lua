02591571ce0a4ca48f9f435f759d891535e263ac
Felipe Nakamoto <felipe.ymn@gmail.com>
2023-10-09 20:36:36 -0300

    feat: update highlight groups

--- lua/florence/palette.lua
+++ lua/florence/palette.lua
@@ -2,7 +2,7 @@ local colors = {
   base00 = "#101317",
   base01 = "#1a1d21",
   base02 = "#23262a",
-  base03 = "#2b2e32",
+  base03 = "#383b3f",
   base04 = "#57606b",
   base05 = "#c5c5c6",
   base06 = "#cbcbcc",
@@ -10,12 +10,12 @@ local colors = {
   base08 = "#d4ae5c",
   base09 = "#78beab",
   base0A = "#e5d487",
-  base0B = "#9BB02E",
+  base0B = "#9bb02e",
   base0C = "#ffa4cb",
   base0D = "#d9573d",
   base0E = "#e69147",
   base0F = "#50799e",
-  r3d = "#FF0000",
+  r3d = "#ff0000",
 }
 
 return colors
