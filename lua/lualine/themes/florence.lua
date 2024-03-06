c93f09bdaedc6c43b1b850ee6b6279ca2f59169f
Felipe Nakamoto <felipe.ymn@gmail.com>
2024-03-05 21:04:07 -0300

    feat: add new highlight groups

--- lua/lualine/themes/florence.lua
+++ lua/lualine/themes/florence.lua
@@ -1,42 +1,43 @@
 local colors = {
-  bg = nil,
-  alt_fg = "#0c0d0a",
-  yellow = "#d4ae5c",
-  blue = "#9fcdd9",
-  cyan = "#78beab",
-  red = "#d9573d",
-  green = "#9bb02e",
-  dark = "#0e0f0c",
+	bg = nil,
+	alt_fg = "#0c0d0a",
+	yellow = "#d4ae5c",
+	blue = "#9fcdd9",
+	cyan = "#78beab",
+	red = "#d9573d",
+	green = "#9bb02e",
+	dark = "#0e0f0c",
+	fg = "#57606b", -- dark foreground (used for status bars), delimiters
 }
 return {
-  normal = {
-    a = { bg = colors.yellow, fg = colors.alt_fg, gui = "bold" },
-    b = { bg = colors.dark, fg = colors.dark },
-    c = { bg = colors.dark, fg = colors.dark },
-  },
-  insert = {
-    a = { bg = colors.blue, fg = colors.alt_fg, gui = "bold" },
-    b = { bg = colors.dark, fg = colors.dark },
-    c = { bg = colors.dark, fg = colors.dark },
-  },
-  visual = {
-    a = { bg = colors.cyan, fg = colors.alt_fg, gui = "bold" },
-    b = { bg = colors.dark, fg = colors.dark },
-    c = { bg = colors.dark, fg = colors.dark },
-  },
-  replace = {
-    a = { bg = colors.red, fg = colors.alt_fg, gui = "bold" },
-    b = { bg = colors.dark, fg = colors.dark },
-    c = { bg = colors.dark, fg = colors.dark },
-  },
-  command = {
-    a = { bg = colors.green, fg = colors.alt_fg, gui = "bold" },
-    b = { bg = colors.dark, fg = colors.dark },
-    c = { bg = colors.dark, fg = colors.dark },
-  },
-  inactive = {
-    a = { bg = colors.dark, fg = colors.dark, gui = "bold" },
-    b = { bg = colors.dark, fg = colors.dark },
-    c = { bg = colors.dark, fg = colors.dark },
-  },
+	normal = {
+		a = { bg = colors.yellow, fg = colors.alt_fg, gui = "bold" },
+		b = { bg = colors.dark, fg = colors.fg },
+		c = { bg = colors.dark, fg = colors.fg },
+	},
+	insert = {
+		a = { bg = colors.blue, fg = colors.alt_fg, gui = "bold" },
+		b = { bg = colors.dark, fg = colors.fg },
+		c = { bg = colors.dark, fg = colors.fg },
+	},
+	visual = {
+		a = { bg = colors.cyan, fg = colors.alt_fg, gui = "bold" },
+		b = { bg = colors.dark, fg = colors.fg },
+		c = { bg = colors.dark, fg = colors.fg },
+	},
+	replace = {
+		a = { bg = colors.red, fg = colors.alt_fg, gui = "bold" },
+		b = { bg = colors.dark, fg = colors.fg },
+		c = { bg = colors.dark, fg = colors.fg },
+	},
+	command = {
+		a = { bg = colors.green, fg = colors.alt_fg, gui = "bold" },
+		b = { bg = colors.dark, fg = colors.fg },
+		c = { bg = colors.dark, fg = colors.fg },
+	},
+	inactive = {
+		a = { bg = colors.dark, fg = colors.fg, gui = "bold" },
+		b = { bg = colors.dark, fg = colors.fg },
+		c = { bg = colors.dark, fg = colors.fg },
+	},
 }
